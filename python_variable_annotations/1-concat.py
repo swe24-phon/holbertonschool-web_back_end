c56d3c14e48eb0219aa26eeb2838f0bef11cbb8f
phon xay <majic42@hotmail.com>
2025-01-15 01:24:26 +1100

    First commit

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Basic annotations - concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Concatenate two strings and return a string """
+
+    return str1 + str2
