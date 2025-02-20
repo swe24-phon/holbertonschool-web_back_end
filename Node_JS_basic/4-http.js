df5846be709d7e4f9385fbce189304eefe1876c7
phon xay <majic42@hotmail.com>
2025-02-21 00:05:39 +1100

    turn into app variable

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,7 +1,8 @@
 const http = require('http');
-http.createServer((req, res) => {
+const app= http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('Hello Holberton School!');
   res.end();
   }).listen(1245);
-  
+
+  module.exports = app
