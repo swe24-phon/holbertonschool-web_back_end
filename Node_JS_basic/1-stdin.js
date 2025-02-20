0385362e2be5db3c41a2f58d0e73646d966c7b54
phon xay <majic42@hotmail.com>
2025-02-20 17:40:55 +1100

    Fix linting error in 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,10 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
-const prompt = process.stdin.read();
+  const prompt = process.stdin.read();
   if (prompt !== null) {
     process.stdout.write(`Your name is: ${prompt}`);
   }
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-}
-);
+});
