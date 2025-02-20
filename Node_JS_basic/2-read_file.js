5dfafdd84753d24b6c5a3ff58f502b83a5bb8c59
phon xay <majic42@hotmail.com>
2025-02-20 19:17:09 +1100

    first commit

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync
+    (path, { encoding: 'utf8' });
+    let students = data.split('\n');
+    students = students.slice(1, students.length - 1);
+    console.log(`Number of students: ${students.length}`);
+    const fields = {};
+    students.forEach((student) => {
+      const field = student.split(',');
+      if (!fields[field[3]]) {
+        fields[field[3]] = [];
+      }
+      fields[field[3]].push(field[0]);
+    });
+    delete fields.field;
+    for (const field in fields) {
+      if (fields[field].length > 0) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    console.error(error)};
+}
+module.exports = countStudents;
