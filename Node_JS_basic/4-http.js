e0de2b675c71424b0ab7f455ccbb6549cf657428
phon xay <majic42@hotmail.com>
2025-02-21 00:10:03 +1100

    fix style

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,8 +1,8 @@
 const http = require('http');
-const app= http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('Hello Holberton School!');
   res.end();
-  }).listen(1245);
-
-  module.exports = app
+}).listen(1245);
+module.exports = app;
