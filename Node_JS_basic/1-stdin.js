294a6a2e81367e6340d7b2cb76810f23c95ea654
phon xay <majic42@hotmail.com>
2025-02-20 17:00:39 +1100

    first commit

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
+  let prompt = process.stdin.read();
+  if (prompt !== null) {
+    process.stdout.write(`Your name is: ${prompt}`);
+  }
+  });
+  process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing\n');
+  }
+);
