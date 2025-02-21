ee54434aa1882fb7a6334fa16c9ec6ba15a659e0
phon xay <majic42@hotmail.com>
2025-02-21 12:28:29 +1100

    modify logic

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,19 +1,23 @@
 const http = require('http');
-const { readFileSync } = require('fs');
 const { countStudents } = require('./3-read_file_async');
+
 const app = http.createServer((req, res) => {
-  res.write(req.url);
   if (req.url === '/') {
-    res.write('Hello Holberton School!');
-    res.end();
-  }
-  else if (req.url === '/students') {
-    res.end('This is the list of our students\n');
-  try {
-    const result = countStudents(process.argv[2]);
-    res.end(`${result.join('\n')}`);
-  } catch(error){
-    res.end('Cannot load the database');
-  }}});
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      countStudents(process.argv[2])
+        .then((result) => {
+          res.end(result.join('\n'));
+        })
+        .catch((error) => {
+          res.end(error.message);
+        });
+    } catch (error) {
+      res.end('Cannot load the database');
+    }
+  }});
+
 app.listen(1245);
 module.exports = app;
