091556c77200cf36c3fe2052e12cfa2b6e6ac5fb
phon xay <majic42@hotmail.com>
2025-02-21 12:21:10 +1100

    modify logic

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,25 +2,18 @@ const http = require('http');
 const { readFileSync } = require('fs');
 const { countStudents } = require('./3-read_file_async');
 const app = http.createServer((req, res) => {
-  res.write(req.url)
+  res.write(req.url);
   if (req.url === '/') {
     res.write('Hello Holberton School!');
     res.end();
   }
   else if (req.url === '/students') {
     res.end('This is the list of our students\n');
-    countStudents(process.argv[2])
-      .then(({ students, studentsByCS, studentsBySWE }) => {
-        res.write('Number of students: ' + students.length + '\n');
-        res.write('Number of students in CS: ' + studentsByCS.length + '. List: ' + studentsByCS.join(', ') + '\n');
-        res.write('Number of students in SWE: ' + studentsBySWE.length + '. List: ' + studentsBySWE.join(', ') + '\n');
-        res.end();
-      })
-      .catch((error) => {
-        res.write('This is the list of our students\n');
-        res.end('Error: ' + error.message);
-      });
-
-  }  
-}).listen(1245);
+  try {
+    const result = countStudents(process.argv[2]);
+    res.end(`${result.join('\n')}`);
+  } catch(error){
+    res.end('Cannot load the database');
+  }}});
+app.listen(1245);
 module.exports = app;
