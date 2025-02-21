0e304460a3c3f9d34d078bf7a61093336a666f09
phon xay <majic42@hotmail.com>
2025-02-21 13:19:19 +1100

    modify logic

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -6,18 +6,22 @@ const app = http.createServer((req, res) => {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
-    try {
-      countStudents(process.argv[2])
-        .then((result) => {
-          res.end(result.join('\n'));
-        })
-        .catch((error) => {
-          res.end(error.message);
-        });
-    } catch (error) {
-      res.end('Cannot load the database');
-    }
-  }});
+    countStudents(process.argv[2])
+      .then((result) => {
+        res.write(`Number of students: ${result.total}\n`);
+        for (const [field, data] of Object.entries(result.fields)) {
+          res.write(`Number of students in ${field}: ${data.count}. List: ${data.list.join(', ')}\n`);
+        }
+        res.end();
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
 
 app.listen(1245);
 module.exports = app;
