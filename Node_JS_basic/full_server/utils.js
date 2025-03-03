0af7f5752b32f6caa2760eb7fb3e3dee97025ed2
phon xay <majic42@hotmail.com>
2025-03-03 21:39:35 +1100

    fix logic

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,37 +1,27 @@
-import fs from 'fs';
-const readDatabase = async (path) => {
-//   try {
-//     const data = await  require(path)
-//     return data;
-//   } catch (error) {
-//     throw new Error('Cannot load the database');
-//   }
-// };
+import fs from "fs";
 
-          
-function readDatabase(filePath) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                const lines = data.trim().split('\n');
-                const fieldNames = lines.shift().split(',');
-                const students = {};
+const readDatabase = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const lines = data.trim().split("\n");
+        const students = {};
 
-                lines.forEach(line => {
-                    const [firstname, , , field] = line.split(',');
-                    if (!students[field]) {
-                        students[field] = [];
-                    }
-                    students[field].push(firstname);
-                });
-
-                resolve(students);
-            }
+        lines.forEach((line) => {
+          const [firstname, , , field] = line.split(",");
+          if (!students[field]) {
+            students[field] = [];
+          }
+          students[field].push(firstname);
         });
+
+        resolve(students);
+      }
     });
-}
+  });
+};
 
 export default readDatabase;
-//module.exports = readDatabase;
+// module.exports = readDatabase;
