4c0d64639a51aefe8c60384c82f53dfc114bd9c1
phon xay <majic42@hotmail.com>
2025-03-05 16:12:55 +1100

    My first ES6

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,6 +1,5 @@
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
-    // income: income, when key and value are the same, you can use the shorthand as one word
     income,
     gdp,
     capita,
