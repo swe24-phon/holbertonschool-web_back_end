86e8ad1e5acef6ee4e45b366333f10cfc335a8a7
phon xay <majic42@hotmail.com>
2025-03-12 22:55:56 +1100

    first push

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -12,11 +12,12 @@ export class HolbertonClass {
     return this._location;
   }
 }
+
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
