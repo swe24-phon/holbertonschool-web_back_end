cf0cd502082d982f459e319a7f2900c0036e8355
phon xay <majic42@hotmail.com>
2025-03-12 23:38:11 +1100

    first push

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,14 +1,14 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') {
-      throw TypeError('Brand must be a string');
-    }
-    if (typeof motor !== 'string') {
-      throw TypeError('Motor must be a string');
-    }
-    if (typeof color !== 'string') {
-      throw TypeError('Color must be a string');
-    }
+    // if (typeof brand !== 'string') {
+    //   throw TypeError('Brand must be a string');
+    // }
+    // if (typeof motor !== 'string') {
+    //   throw TypeError('Motor must be a string');
+    // }
+    // if (typeof color !== 'string') {
+    //   throw TypeError('Color must be a string');
+    // }
 
     this._brand = brand;
     this._motor = motor;
