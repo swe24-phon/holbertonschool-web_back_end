4627b4301cb38e8f9beff3e6754ae6e3916e162d
phon xay <majic42@hotmail.com>
2025-03-13 00:16:03 +1100

    first commit

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,10 @@
 function getListStudents() {
-  students = [
-    { id: 1, name: 'Guillaume', location: 'San Francisco' },
-    { id: 2, name: 'Jame', location: 'Columbia' },
-    { id: 5, name: 'Serena', location: 'San Francisco' },
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
   return students;
 }
+
+export default getListStudents;
---
+++ ES6_data_manipulation/0-get_list_students.test.js
@@ -0,0 +1,9 @@
+import getListStudents from './0-get_list_students';
+
+test('getListStudents is implemented correctly', () => {
+  expect(getListStudents()).toEqual([
+    { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
+    { firstName: 'James', id: 2, location: 'Columbia' },
+    { firstName: 'Serena', id: 5, location: 'San Francisco' },
+  ]);
+});
