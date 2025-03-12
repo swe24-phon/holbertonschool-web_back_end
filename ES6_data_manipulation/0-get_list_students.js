20d2d49beda7b6cd269435f643865767771ad1d8
phon xay <majic42@hotmail.com>
2025-03-13 00:04:52 +1100

    first commit

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+function getListStudents() {
+  students = [
+    { id: 1, name: 'Guillaume', location: 'San Francisco' },
+    { id: 2, name: 'Jame', location: 'Columbia' },
+    { id: 5, name: 'Serena', location: 'San Francisco' },
+  ];
+  return students;
+}
