989efbe1d5f02a21d6fbed0588bc99ba6ee3f91c
phon xay <majic42@hotmail.com>
2025-03-12 18:38:15 +1100

    First commit

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
-import { ClassRoom } from './0-classroom.js';
+import ClassRoom from './0-classroom.js';
 
-function initializeRooms() {
+export default function initializeRooms() {
   const rooms = [];
   rooms.push(new ClassRoom(19));
   rooms.push(new ClassRoom(20));
