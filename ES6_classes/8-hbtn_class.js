95fabcf8c68e173130b69f5c3dac3630926e4d59
phon xay <majic42@hotmail.com>
2025-03-12 22:34:36 +1100

    first push

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,28 @@
+class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('location must be a string');
+    }
+
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') return this._size;
+    if (hint === 'string') return this._location;
+    return null;
+  }
+}
+export default HolbertonClass;
