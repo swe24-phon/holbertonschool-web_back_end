d67aa83a99fe68f2437ecd5f0ba6e4252a7d08ca
phon xay <majic42@hotmail.com>
2025-03-12 19:23:55 +1100

    first push

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,4 +1,4 @@
-class Currency {
+export default class Currency {
   constructor(code, name) {
     if (typeof code !== 'string') {
       throw TypeError('Code must be a string');
@@ -8,6 +8,28 @@ class Currency {
     }
     this._code = code;
     this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    this._code = newCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newname) {
+    if (typeof newname !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = newname;
   }
 
   displayFullCurrency() {
