173b672522f780616995ea125fa49d19dea57280
phon xay <majic42@hotmail.com>
2025-03-12 19:56:40 +1100

    add convertPrice

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,26 @@
+class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw TypeError('Square footage must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  //abstract method
+  evacuationWarningMessage() {
+    throw new Error(
+      'Class extending Building must override evacuationWarningMessage',
+    );
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw TypeError('Square footage must be a number');
+    }
+    this._sqft = newSqft;
+  }
+}
