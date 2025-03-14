5ad6bd36aa9cbba51c613b71e98192205bd4a0a6
phon xay <majic42@hotmail.com>
2025-03-15 00:42:50 +1100

    first

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,13 +2,20 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName)
-    .then((value) => ({ status: 'fulfilled', value }))
-    .catch((error) => ({ status: 'rejected', value: error }));
+  const userPromise = signUpUser(firstName, lastName).then(
+    (value) => ({ status: 'fulfilled', value }),
+    (error) => ({ status: 'rejected', value: error.toString() }), // Ensure error is converted to a string
+  );
 
-  const photoPromise = uploadPhoto(fileName)
-    .then((value) => ({ status: 'fulfilled', value }))
-    .catch((error) => ({ status: 'rejected', value: error }));
+  const photoPromise = uploadPhoto(fileName).then(
+    (value) => ({ status: 'fulfilled', value }),
+    (error) => ({ status: 'rejected', value: error.toString() }), // Convert error to string
+  );
 
-  return Promise.all([userPromise, photoPromise]);
+  return Promise.allSettled([userPromise, photoPromise]).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value: result.reason || result.value, // Include either reason or value
+    })),
+  );
 }
