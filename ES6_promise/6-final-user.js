3fa7e325f64119888aacecbaee6bec81585f5652
phon xay <majic42@hotmail.com>
2025-03-15 00:40:19 +1100

    first

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,22 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // Call both functions and handle their promises
-  const userPromise = signUpUser(firstName, lastName).then(
-    (value) => ({ status: 'fulfilled', value }),
-    (error) => ({ status: 'rejected', value: error }),
-  );
+  const userPromise = signUpUser(firstName, lastName)
+    .then((value) => ({ status: 'fulfilled', value }))
+    .catch((error) => ({ status: 'rejected', value: error }));
 
-  const photoPromise = uploadPhoto(fileName).then(
-    (value) => ({ status: 'fulfilled', value }),
-    (error) => ({ status: 'rejected', value: error }),
-  );
+  const photoPromise = uploadPhoto(fileName)
+    .then((value) => ({ status: 'fulfilled', value }))
+    .catch((error) => ({ status: 'rejected', value: error }));
 
-  // Return an array of promise results once all promises are settled
-  return Promise.allSettled([userPromise, photoPromise]).then((results) =>
-    results.map((result) => ({
-      status: result.status,
-      value: result.reason || result.value,
-    })),
-  );
+  return Promise.all([userPromise, photoPromise]);
 }
