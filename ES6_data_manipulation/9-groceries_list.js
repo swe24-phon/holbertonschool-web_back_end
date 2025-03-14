dc43e6d1b9071e5915ddf5457450fb529fb2300c
phon xay <majic42@hotmail.com>
2025-03-14 19:16:15 +1100

    first commit

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+export default function groceriesList() {
+  fruits= [
+    ['apple', 10],
+    [Tomatoes, 10],
+    [Pasta, 1],
+    [Rice, 1],
+    [Banana, 5],
+    ['banana', 5],
+  ];
+
+  fruits.forEach((item, c) => {
+    fruits.map(item => item[0].toLowerCase());
+}
