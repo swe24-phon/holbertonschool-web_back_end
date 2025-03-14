3ef270d814d0bb8ac6275794cbddb3c0f0c7d448
phon xay <majic42@hotmail.com>
2025-03-14 18:42:21 +1100

    first commit

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(sett, startString) {
+  if (!(sett instanceof Set)) {
+    throw new Error('Input is not a set');
+  }
+  if (typeof startString !== 'string') {
+    throw new Error('Start string is not a string');
+  }
+  const cleanedSet = new Set();
+  sett.forEach((item) => {
+    if (typeof item === 'string' && item.startsWith(startString)) {
+      cleanedSet.add(`${item}` + '-');
+    }
+  });
+  return cleanedSet;
+}
