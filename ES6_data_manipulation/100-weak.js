b0de064f4e9cf09aaf9778f983450048e2ef296c
phon xay <majic42@hotmail.com>
2025-03-14 20:42:22 +1100

    first commit

--- ES6_data_manipulation/100-weak.js
+++ ES6_data_manipulation/100-weak.js
@@ -1,13 +1,13 @@
 export const weakMap = new WeakMap();
 
-export default function queryAPI(endpoint) {
-  if (WeakMap.has(endpoint)) {
-    let callCount = WeakMap.get(endpoint) + 1;
-    WeakMap.set(endpoint, callCount);
+export function queryAPI(endpoint) {
+  if (weakMap.has(endpoint)) {
+    let callCount = weakMap.get(endpoint) + 1;
+    weakMap.set(endpoint, callCount);
     if (callCount >= 5) {
       throw new Error('Endpoint load is high');
-    } else {
-      weakMap.set(endpoint, 1);
     }
+  } else {
+    weakMap.set(endpoint, 1);
   }
 }
