9667bffc4df1e08d28a733c7ce990cf306ed43de
phon xay <majic42@hotmail.com>
2025-03-14 23:39:44 +1100

    first

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,4 +1,4 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log('Got a response from the API');
