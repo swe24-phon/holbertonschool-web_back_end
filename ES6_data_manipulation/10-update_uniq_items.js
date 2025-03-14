7924b22a3472bde92a580151fe94fa643579e2c9
phon xay <majic42@hotmail.com>
2025-03-14 19:56:08 +1100

    first commit

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+import groceriesList from './9-groceries_list';
+
+export default function updateUniqItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+  return map;
+}
