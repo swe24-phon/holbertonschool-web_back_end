9c869cacd6c15105050c6d3f5858f5dea6c92ba4
phon xay <majic42@hotmail.com>
2025-03-14 18:19:19 +1100

    First commit

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,6 @@
 export default function hasArrayValues(sett, arr) {
+  if (!Array.isArray(arr) || !Array.isArray(sett)) {
+    throw new Error('Input is not an array');
+  }
   return sett.some((item) => arr.includes(item));
 }
