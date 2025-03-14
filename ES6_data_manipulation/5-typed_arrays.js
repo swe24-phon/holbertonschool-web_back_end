57725aa63acb15f163d86116baf370a454c31b6b
phon xay <majic42@hotmail.com>
2025-03-14 17:22:24 +1100

    First commit

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
-function createInt8TypedArray(length, position, value) {
+export default function createInt8TypedArray(length, position, value) {
   if (position >= length || position < 0) {
     throw new Error('Position outside range');
   }
 
-  const buffer = new ArrayBuffer(length);
-  const view = new DataView(buffer);
+  const arr = new Int8Array(length);
+  const view = new DataView(arr.buffer);
   view.setInt8(position, value);
 
-  return view;
+  return arr;
 }
