00feb334026965625619992a86bfe829401acb5f
phon xay <majic42@hotmail.com>
2025-03-15 08:41:41 +1100

    first

---
+++ ES6_promise/100-await.js
@@ -0,0 +1,7 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  const photo = await uploadPhoto();
+  const user = await createUser();
+  return { photo, user };
+}
