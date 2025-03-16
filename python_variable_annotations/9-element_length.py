7aee6d19c869009a44d351c936c29518fd7e2737
phon xay <majic42@hotmail.com>
2025-03-17 01:44:27 +1100

    First commit

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,9 +2,10 @@
 """
 This function takes a list of strings and returns a list of
 tuples containing the string and its length."""
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: list[str]) -> list[tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return a list of tuples containing the string and
     its length for each string in the input list."""
     return [(i, len(i)) for i in lst]
