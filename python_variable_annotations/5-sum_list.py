62dc230a54d7e44afbb8530062e1e2c624774c6d
phon xay <majic42@hotmail.com>
2025-03-17 01:08:19 +1100

    First commit

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Sum of a list of floats."""
 
+from typing import List
 
-def sum_list(input_list: float) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """Sum of a list of floats."""
     return sum(input_list)
