71baefa15d4399c98394c983352c0526214c5c90
phon xay <majic42@hotmail.com>
2025-03-17 00:39:22 +1100

    First commit

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' this module takes a string str1 and a string str2 as arguments and
+returns their concatenated string'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''returns the concatenated string of str1 and str2'''
+    return str1 + str2
