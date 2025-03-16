1640b7af7348239bf8b8aa21674dc3dc7263adfa
phon xay <majic42@hotmail.com>
2025-03-17 01:11:55 +1100

    First commit

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Sum of a list of floats and integers."""
+
+
+def sum_mixed_list(input_list: list) -> float:
+    """Sum of a list of floats and integers."""
+    return sum(input_list)
