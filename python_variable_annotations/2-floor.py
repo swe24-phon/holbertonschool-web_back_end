8e1cc32886c95d1d0c8ca409f33ef7f61cf83e7d
phon xay <majic42@hotmail.com>
2025-03-17 00:44:48 +1100

    First commit

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' this module takes a float a and a float b as arguments and
+returns their sum as a float'''
+import math
+
+def floor(n: float) -> float:
+    '''returns the sum of a and b'''
+    return math.floor(n)
