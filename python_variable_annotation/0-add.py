00ef59d701a533af3ed29b7417c5eba5700da876
Phonexay soumpholphakdy <majic42@hotmail.com>
2025-03-17 00:31:17 +1100

    Delete python_variable_annotation directory

--- python_variable_annotation/0-add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-''' this module takes a float a and a float b as arguments and
-returns their sum as a float'''
-
-def add (a: float, b: float) -> float:
-    ''' this function takes two float arguments and returns their sum'''
-    return a + b
