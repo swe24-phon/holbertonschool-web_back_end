63ccfef3529d1e66c8152e1bdbe0ba5915ab710f
phon xay <majic42@hotmail.com>
2025-03-17 00:21:44 +1100

    First commit

--- python_variable_annotation/0-add.py
+++ python_variable_annotation/0-add.py
@@ -1,2 +1,7 @@
-function add (a: float, b: float) -> float:
+#!/usr/bin/env python3
+''' this module takes a float a and a float b as arguments and
+returns their sum as a float'''
+
+def add (a: float, b: float) -> float:
+    ''' this function takes two float arguments and returns their sum'''
     return a + b
