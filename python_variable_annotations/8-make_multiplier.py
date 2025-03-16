b3cb75defd9dcd6399655bfd0e1c8ec3097fd6af
phon xay <majic42@hotmail.com>
2025-03-17 01:30:17 +1100

    First commit

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,8 +2,10 @@
 
 """Type-annotated function make_multiplier that takes a float multiplier as argument and returns a function that multiplies a float by multiplier."""
 
+from typing import Callable
 
-def make_multiplier(multiplier: float) -> callable:
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Type-annotated function make_multiplier that takes a float multiplier as argument and returns a function that multiplies a float by multiplier."""
 
     def multiply(n: float) -> float:
