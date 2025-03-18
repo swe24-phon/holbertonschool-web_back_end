6002c7ba800a2e11e15f29618200af26e492020a
phon xay <majic42@hotmail.com>
2025-03-18 13:03:20 +1100

    First commit

---
+++ python_async_function/lesson/coroutine.py
@@ -0,0 +1,15 @@
+import asyncio
+
+async def say_hello():
+    await asyncio.sleep(1)
+    print("Hello")
+
+async def say_world():
+    await asyncio.sleep(1)
+    print("World")
+
+async def main():
+    await say_hello()
+    await say_world()
+
+asyncio.run(main())
